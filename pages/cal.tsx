fa98134dd8ca310927b3176dbad218d729f30896
Jim Puls <jim@nondifferentiable.com>
2021-08-13 16:45:39 -0700

    Move to next.js

---
+++ pages/cal.tsx
@@ -0,0 +1,71 @@
+import Head from "next/head";
+
+function CalendarPage() {
+  return (
+    <main>
+      <Head>
+        <title>Dear Recruiters:</title>
+      </Head>
+      <h1>Dear Recruiters:</h1>
+      <p>
+        Hi! It's your friendly local software engineer. You probably came across
+        this link because you cold-emailed or cold-called me as a prospective
+        candidate, and I asked you to visit this in lieu of a longer response.
+      </p>
+      <p>
+        The talent crunch is worse than ever: the tech industry has grown by
+        leaps and bounds, the FANMAGs have driven salaries sky high, and you
+        have an imperative to hire infinity plus one engineers for your
+        organization.
+      </p>
+      <p>
+        Even those of us who are happily employed right now are aware of this
+        and generally ready to hear about the next big thing, especially when
+        the market is so firmly on our side.
+      </p>
+      <p>
+        <strong>It's the most exciting time yet for all of us.</strong>
+      </p>
+      <p>
+        But here's the rub: now we're all so busy that instead of even trying to
+        engage us in a conversation or some sort of dialogue, your cold emails
+        just have links to your Calendly.
+      </p>
+      <p>
+        This sets up a dynamic where you're asking us to jump through hoops just
+        to talk to you.
+      </p>
+      <p>
+        One where we're just a resource to be acquired instead of a person with
+        priorities, needs, and—most certainly—an ego.
+      </p>
+      <p>
+        When all we hear from you is "please set up a time on my calendar if you
+        want to discuss opportunities with my company", we feel fungible.
+      </p>
+      <p>Faceless.</p>
+      <p>
+        Wherever we are now, our company at the very least views us as an
+        individual. Sending such an impersonal intro doesn't really make the
+        opportunity you have for us very appealing in comparison.
+      </p>
+      <p>
+        <strong>
+          Please engage us in a conversation instead of just dumping us straight
+          to your calendar.
+        </strong>
+        &nbsp; That's all we ask.
+      </p>
+      <hr />
+      <p className="footer">
+        <a href="http://dearrecruiters.com">DearRecruiters.com</a> is a free
+        service from <a href="http://twitter.com/puls">@puls</a>. Shout out to
+        <a href="http://thanksno.com">ThanksNo.com</a> for the inspiration.
+        Obviously, the prospective candidate you emailed didn't personally write
+        this, but it's the sentiment that counts.
+      </p>
+    </main>
+  );
+}
+
+export default CalendarPage;
